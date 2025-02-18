ece6cadc579cb67103ea98b6ca2e935b6718410f
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2025-02-19 00:41:12 +0530

    Initial Commit

---
+++ webviewSDK/webviewSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewSDKApp.swift
+//  webviewSDK
+//
+//  Created by Chetan Mittal on 2025/02/19.
+//
+
+import SwiftUI
+
+@main
+struct webviewSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ webviewSDKTests/webviewSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  webviewSDKTests.swift
+//  webviewSDKTests
+//
+//  Created by Chetan Mittal on 2025/02/19.
+//
+
+import Testing
+@testable import webviewSDK
+
+struct webviewSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
